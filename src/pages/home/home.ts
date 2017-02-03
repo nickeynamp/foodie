4396597d88ed2851d88c06318423dfd981a44456
Nick Pham <nickp@Nicks-MacBook-Air.local>
2017-02-03 01:52:12 +0100

    Basics

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { App,MenuController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-    
+  constructor(app:App, menu:MenuController){
+    menu.enable(true);
   }
-
 }
+
+// @Component({
+//   templateUrl: 'home.html'
+// })
+// export class searchPage{
+
+// }
+// @Component({
+//   templateUrl: 'home.html'
+// })
+
+// export class profile{
+
+// }
+
