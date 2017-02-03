d6bd1fdfa5324ab26ab83d2f23efb3d9bce58fe0
Nick Pham <nickp@n142-p253.kthopen.kth.se>
2017-02-03 18:04:04 +0100

    Navigation buttons fixed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 
 import { App,MenuController } from 'ionic-angular';
-import { BrowsePage } from '../pages/browse/browse';
+import { BrowsePage } from '../browse/browse';
+import { RestaurantPage } from '../restaurant/restaurant';
+import { InboxPage } from '../inbox/inbox';
+import { UserPage } from '../user/user';
+import { MessagePage } from '../message/message';
 
 @Component({
   selector: 'page-home',
@@ -10,6 +14,10 @@ import { BrowsePage } from '../pages/browse/browse';
 
 export class HomePage {
   browsePage = BrowsePage;
+  restaurantPage = RestaurantPage;
+  inboxPage = InboxPage;
+  userPage = UserPage;
+  messagePage = MessagePage;
 
   constructor(app:App, menu:MenuController){
     menu.enable(true);
