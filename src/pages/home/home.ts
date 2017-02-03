e2109d5dcd18450c2d76a28dfb118602742807ed
Nick Pham <nickp@n142-p253.kthopen.kth.se>
2017-02-03 17:52:29 +0100

    adding navigation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { App,MenuController } from 'ionic-angular';
+import { BrowsePage } from '../pages/browse/browse';
 
 @Component({
   selector: 'page-home',
@@ -8,7 +9,11 @@ import { App,MenuController } from 'ionic-angular';
 })
 
 export class HomePage {
+  browsePage = BrowsePage;
+
   constructor(app:App, menu:MenuController){
     menu.enable(true);
   }
+
+
 }
